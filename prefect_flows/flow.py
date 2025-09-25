3f7aece108b5d5c2087ed1cfe20c35305d8071f7
Issa KABORE <kikikabore1@gmail.com>
2025-09-25 14:40:08 +0200

    Added: Pipeline openaq

---
+++ prefect_flows/flow.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+import smartcity
+from smartcity.database.functions import upsert_measurements
+from smartcity.air_quality.openaq_api import fetch_openaq_data
+
+from prefect import flow, tags, task
+from prefect.logging import get_run_logger
+
+
+@task(retries=3, retry_delay_seconds=10)
+def fetch_openaq() -> pd.DataFrame:
+    """Fetch air quality data from OpenAQ API"""
+    data = fetch_openaq_data()
+    return data
+
+
+@flow(name="SmartCity OpenAQ ETL", log_prints=True)
+def smartcity_openaq_etl():
+    """
+    ETL flow: fetch air quality data from OpenAQ and upsert into Supabase.
+    Designed to run daily via Prefect Cloud.
+    """
+    log = get_run_logger()
+    log.info("Starting SmartCity OpenAQ ETL flow ...")
+    log.info(f">>> {smartcity.__version__ =  }")
+    df = fetch_openaq_data()
+    if df.empty:
+        log.warning("No data fetched from OpenAQ.")
+        return
+
+    upsert_measurements(df)
