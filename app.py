971c56f5e8b796b1a75ed6f94cb801eead7cadc7
Issa KABORE <kikikabore1@gmail.com>
2025-10-09 17:22:24 +0200

    fix

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import subprocess
 import sys
 
 if __name__ == "__main__":
-    subprocess.run([sys.executable, "-m", "streamlit", "run", "streamlit_app/Home.py", "--server.port=7860", "--server.address=0.0.0.0"])
+    subprocess.run([sys.executable, "-m", "streamlit", "run", "streamlit_app/Home.py"])
